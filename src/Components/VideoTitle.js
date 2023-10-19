d683512221928ff6cb49340b3efa40d32f7cd74f
kartikpandit79 <kartik.pandit79@gmail.com>
2023-10-19 13:38:47 +0530

    correction in play button

--- src/Components/VideoTitle.js
+++ src/Components/VideoTitle.js
@@ -7,9 +7,9 @@ const VideoTitle = ({ title, overview }) => {
             <h1 className='text-xl md:text-6xl font-bold'>{title}</h1>
             <p className='hidden md:inline-block py-6 w-1/4 text-sm'>{overview}</p>
             <div className=' flex mt-[10px] md:m-0'>
-                <button className=' flex mx-2 font-bold bg-white text-black py-1 md:py-4 px-2 md:px-12 text-sm md:text-2xl rounded-lg hover:bg-opacity-80 '>
-                    <img className='w-[1.2rem] p-1' src={PLAY_BUTTON} /> Play</button>
-                <button className=' hidden md:inline-block mx-2 bg-gray-500 text-white p-4 px-12 text-2xl bg-opacity-50 rounded-lg hover:bg-opacity-20'>More Info</button>
+                <button className=' flex mx-2 font-bold bg-white text-black py-1 md:py-[1rem] px-2 md:px-[1.5rem] text-sm md:text-2xl rounded-lg hover:bg-opacity-80 '>
+                    <img className='w-[1.2rem] md:w-6 p-1 md:mt-[.4rem] mt-0' src={PLAY_BUTTON} /> Play</button>
+                <button className=' hidden md:inline-block mx-2 bg-gray-500 text-white p-4 px-[1.5rem] text-2xl bg-opacity-50 rounded-lg hover:bg-opacity-20'>More Info</button>
             </div>
         </div>
     )
